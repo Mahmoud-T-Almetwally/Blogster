3cb76946cc12797ba2d7fc41d56460094dc2f8a5
Mahmoud-T-Almetwally <mahmoudtariq@std.mans.edu.eg>
2024-04-25 18:29:12 +0200

    All html files now inhereit from base

--- app.py
+++ app.py
@@ -7,7 +7,23 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return render_template('PostTemplate.html')
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def login():
+    return render_template('contact.html')
+
+@app.route('/signup')
+def login():
+    return render_template('signup.html')
+
+@app.route('/about')
+def login():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
