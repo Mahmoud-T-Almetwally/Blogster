8089f1ac6cd341a17d5ba306f86718cdba3fb33b
Mahmoud-T-Almetwally <mahmoudtariq@std.mans.edu.eg>
2024-04-25 14:00:30 +0200

    Added Base.html

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
