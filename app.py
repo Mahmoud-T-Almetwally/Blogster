6cda4f28a88875c8acb007552f9ce2a629b13854
Mahmoud-T-Almetwally <mahmoudtariq@std.mans.edu.eg>
2024-04-26 17:24:16 +0300

    Added ERD and DB folders & files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,6 +24,10 @@ def signup():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/Post/<int:id>')
+def get_post(id):
+    return render_template("PostTemplate.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
