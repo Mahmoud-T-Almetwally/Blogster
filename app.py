45368f34e1cb84b5496fd10e6b6e3f182ff3444d
Mahmoud-T-Almetwally <mahmoudtariq@std.mans.edu.eg>
2024-04-27 08:54:55 +0300

    Added Register Functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-from utils import validate_login
+from utils import validate_login, username_availabe, match_passwords, email_availble, register_user
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def index():
     else:
         return render_template('index.html')
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=['GET', "POST"])
 def login():
     if request.method == "POST": 
         valid, user = validate_login(request.form['email'], request.form['password'])
@@ -18,6 +18,7 @@ def login():
             resp = redirect(url_for('index'))
             resp.set_cookie('LoggedIn', 'True')
             resp.set_cookie('Username', user[1])
+            resp.set_cookie('User_id', user[0])
             return resp
     return render_template('login.html')
 
@@ -28,12 +29,29 @@ def contact():
     else:
         return render_template('contact.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if bool(request.cookies.get('LoggedIn')):
-        return render_template('signup.html', LoggedIn=bool(request.cookies.get('LoggedIn')), Username="Hello, " + request.cookies.get('Username'))
+    if request.method == "POST":
+        if not username_availabe(request.form['username']):
+            return render_template('signup.html', user_not_avail=True)
+            
+        if not match_passwords(request.form['password'], request.form['conf_password']):
+            return render_template('signup.html', passwords_dont_match=True)
+        
+        if not email_availble(request.form['email']):
+            return render_template('signup.html', email_taken = True)
+        
+        register_user(request.form['username'], request.form['password'], request.form['number'], request.form['email'])
+        _, user = validate_login(request.form['email'], request.form['password'])
+        resp = redirect(url_for('index'))
+        resp.set_cookie('LoggedIn', 'True')
+        resp.set_cookie('Username', user[1])
+        resp.set_cookie('User_id', str(user[0]))
+        return resp
     else:
         return render_template('signup.html')
+        
+    
 
 @app.route('/about')
 def about():
@@ -42,12 +60,6 @@ def about():
     else:
         return render_template('about.html')
 
-@app.route('/Post/<int:post_id>')
-def get_post(post_id):
-    if bool(request.cookies.get('LoggedIn')):
-        return render_template('Post.html', LoggedIn=bool(request.cookies.get('LoggedIn')), Username="Hello, " + request.cookies.get('Username'))
-    else:
-        return render_template('Post.html')
 
 @app.route('/Profile')
 def Profile():
@@ -61,7 +73,16 @@ def Logout():
     resp = redirect(url_for('index'))
     resp.delete_cookie("Username")
     resp.delete_cookie('LoggedIn')
+    resp.delete_cookie('User_id')
     return resp
+
+@app.route('/Post/<int:post_id>')
+def get_post(post_id):
+
+    if bool(request.cookies.get('LoggedIn')):
+        return render_template('Post.html', LoggedIn=bool(request.cookies.get('LoggedIn')), Username="Hello, " + request.cookies.get('Username'))
+    else:
+        return render_template('Post.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -9,3 +9,37 @@ def validate_login(email, password):
     valid = user[2] == password
     conn.close()
     return valid, user
+
+def get_post(post_id: int):
+    pass
+
+def get_posts(post_id: list, num: int):
+    pass
+
+def register_user(username, password, phone, email):
+    conn = sqlite3.connect('DB/blog.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO Users (user_Name, password, phone, email) VALUES (:username, :password, :phone, :email)',
+               {'username': username, "password": password, "phone": phone, "email":email})
+    conn.commit()
+    conn.close()
+
+def username_availabe(username):
+    conn = sqlite3.connect('DB/blog.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM Users WHERE user_Name=:username", {'username': username})
+    users = c.fetchall()
+    conn.close()
+    return (len(users) == 0)
+
+def match_passwords(password, conf_password):
+    return password == conf_password
+
+def email_availble(email):
+    conn = sqlite3.connect('DB/blog.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM Users WHERE email=:email", {'email': email})
+    print('User Added')
+    users = c.fetchall()
+    conn.close()
+    return (len(users) == 0)
